4ffc226377863743d35b4b5823a61c6d238e5437
Wendal Chen <wendal1985@gmail.com>
2023-12-15 15:09:07 +0800

    fix: iotauth修改后编译失败

--- xmake.lua
+++ xmake.lua
@@ -120,7 +120,8 @@ target("luatos-lua")
     add_files(luatos.."components/mbedtls/library/*.c")
     add_includedirs(luatos.."components/mbedtls/include")
     -- iotauth
-    add_files(luatos.."components/iotauth/luat_lib_iotauth.c")
+    add_includedirs(luatos.."components/iotauth")
+    add_files(luatos.."components/iotauth/*.c")
     -- crypto
     add_files(luatos.."components/crypto/**.c")
     -- protobuf
