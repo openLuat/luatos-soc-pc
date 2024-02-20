f8dd22984599ddbff1c01205765c6a8c1f6255f9
Wendal Chen <wendal1985@gmail.com>
2024-02-20 10:05:10 +0800

    update: 验证libgnss分段解析能力

--- test/017.libgnss_hour0/main.lua
+++ test/017.libgnss_hour0/main.lua
@@ -10,6 +10,18 @@ sys.taskInit(function()
     libgnss.parse(str)
     log.info("rmc", json.encode(libgnss.getRmc(), "7f"))
 
+    print("==========================================================================")
+    
+    libgnss.clear()
+    str, str2 = "$GNRMC,003657.000,A",",2324.3944811,N,11313.8621125,E,0.430,43.152,020124,,,A,U*32\r\n"
+    libgnss.parse(str)
+    libgnss.parse(str2)
+    log.info("rmc分段传入", json.encode(libgnss.getRmc(), "7f"))
+    libgnss.clear()
+    
+    libgnss.parse(str .. str2)
+    log.info("rmc一次传入", json.encode(libgnss.getRmc(), "7f"))
+    libgnss.clear()
 end)
 
 sys.run()
