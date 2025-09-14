59ca6fa3bf2f7c0f17050f6e092da98ba0bf0530
PKM-Bai <1242013107@qq.com>
2025-09-15 01:19:19 +0800

    add: airui,添加绑定动态变量和绑定函数的使用例子

--- test/088.airui/main.lua
+++ test/088.airui/main.lua
@@ -2,8 +2,30 @@
 _G.sys = require("sys")
 
 
-sys.taskInit(function()
+local num = 10
+local username = "张三"
+local appdata = {
+    -- age = num,
+    person = {
+        name = username,
+        age = num
+    }
+}
 
+local function event_handler(obj, event)
+    if (event == lvgl.EVENT_CLICKED) then
+        appdata.person.age = appdata.person.age + 1
+        airui.refresh_text("text_area_2", appdata)
+        log.info("button clicked age=", appdata.person.age)
+    end
+end
+
+local appfuncs = {
+    update_age = event_handler
+}
+
+
+sys.taskInit(function()
     -- local uiJson = io.open("/luadb/ui.json")
     -- local ui_path = "/luadb/ui.json"
     local ui_path = "/luadb/demo_2.json"
@@ -12,7 +34,11 @@ sys.taskInit(function()
     log.info("ui", ui, ui.pages[1].children[1].name)
     log.info("初始化lvgl", lvgl.init(ui.project_settings.resolution.width, ui.project_settings.resolution.height))
     
-    airui.init(ui_path)
+    airui.init(ui_path, {data = appdata, funcs = appfuncs})
+
+    local button = airui.get_widget("button_2")
+    sys.wait(5000)
+    -- airui.del_widget(button)
 end)
 
 
