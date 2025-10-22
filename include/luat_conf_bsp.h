78677e7e74aa89f4de6d69914d5ce4a02b231b03
Wendal Chen <wendal1985@gmail.com>
2025-10-22 15:31:58 +0800

    add: vm,版本号改到V2009

--- include/luat_conf_bsp.h
+++ include/luat_conf_bsp.h
@@ -4,7 +4,7 @@
 
 #include "stdint.h"
 
-#define LUAT_BSP_VERSION "V2008"
+#define LUAT_BSP_VERSION "V2009"
 // #define LUAT_CONF_USE_LIBSYS_SOURCE 1
 #define LUAT_USE_CMDLINE_ARGS 1
 // 启用64位虚拟机
@@ -118,7 +118,7 @@
 #define LUAT_USE_PROFILER 1
 
 // #define LUAT_USE_ROSTR 1
-// #define LUAT_USE_VTOOL 1
+#define LUAT_USE_VTOOL 1
 
 //--------------------------------------------------
 // GUI相关
